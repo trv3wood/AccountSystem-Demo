d8b908073b1de0f9918e87ddee5fadf4145c7c16
Z YS <1841369469@qq.com>
2024-05-01 01:54:48 +0800

    2024/5/1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setAttribute(Qt::WA_TranslucentBackground, true);//设置窗口背景透明
 }
 
 MainWindow::~MainWindow()
