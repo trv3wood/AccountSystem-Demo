bb6f40efad9c0e9f6f55387612dbe3e5049bb909
Z YS <1841369469@qq.com>
2024-05-01 12:29:39 +0800

    change img&button color

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    setAttribute(Qt::WA_TranslucentBackground, true);//设置窗口背景透明
+    ui->vLayoutButton->setAlignment(Qt::AlignCenter);
 }
 
 MainWindow::~MainWindow()
