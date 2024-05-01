e693591dfaa35ebe383b1123bc3263e315d8314c
Z YS <1841369469@qq.com>
2024-05-01 18:36:59 +0800

    2024/5/1 18:35 圆角按钮

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QGraphicsEffect>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -7,6 +8,14 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     ui->vLayoutButton->setAlignment(Qt::AlignCenter);
+    // 创建阴影效果
+    QGraphicsDropShadowEffect *shadow = new QGraphicsDropShadowEffect();
+    shadow->setBlurRadius(10);
+    shadow->setXOffset(5);
+    shadow->setYOffset(5);
+
+    ui->buttonLogin->setGraphicsEffect(shadow);
+    ui->buttonSignUp->setGraphicsEffect(shadow);
 }
 
 MainWindow::~MainWindow()
