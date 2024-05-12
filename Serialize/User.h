dd00f05080a27539fc1d95917928524f157cd448
Z YS <1841369469@qq.com>
2024-05-12 20:04:00 +0800

    修改初始化，成员变量重命名，运行时错误

--- Serialize/User.h
+++ Serialize/User.h
@@ -6,7 +6,11 @@
 
 class User : public bms::Serializable {
 public:
-    User(const QString& name = "", int age = 0) : name(name), age(age) {}
+    User(const QString& name = "", int age = 0) : m_name(name), m_age(age) {
+        QString userFileName = name + ".dat";
+        QFile file(userFileName);
+        serialize(file);
+    }
     User(QFile& file) {
         deserialize(file);
     }
@@ -18,31 +22,31 @@ public:
     void serialize(QFile& file) const override {
         QDataStream ds;
         if (!file.open(QIODevice::WriteOnly)) {
-            std::cerr << "Failed to open file for writing" << '\n';
+            throw std::runtime_error("Failed to open file for writing");
         }
         ds.setDevice(&file);
-        ds << name << age;
+        ds << m_name << m_age; 
         file.close();
     }
 
     Serializable* deserialize(QFile& file) override {
         if (!file.open(QIODevice::ReadOnly)) {
-            std::cerr << "Failed to open file for reading" << '\n';
+            throw std::runtime_error("Failed to open file for reading");
         }
         QDataStream ds;
         ds.setDevice(&file);
-        ds >> name >> age;
+        ds >> m_name >> m_age;
         file.close();
         return this;
     }
 
     friend std::ostream& operator<<(std::ostream& os, const User& user) {
-        os << user.name.toStdString() << " " << user.age;
+        os << user.m_name.toStdString() << " " << user.m_age;
         return os;
     }
 
 
 private:
-    QString name;
-    int age;
+    QString m_name;
+    int m_age;
 };
--- Serialize/test.cpp
+++ Serialize/test.cpp
@@ -3,7 +3,7 @@ int main() {
     User user("John Ditor", 17);
     QFile file("user.dat");
     user.serialize(file);
-    User* user2 = dynamic_cast<User*>(user.deserialize(file));
-    std::cout << *user2 << '\n';
+    User user2(file);
+    std::cout << user2 << '\n';
     return 0;
 }
