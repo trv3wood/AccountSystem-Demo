ad8bd4b9dd70a16ba372e9666780a78b5a8d9d79
Z YS <1841369469@qq.com>
2024-05-12 18:09:31 +0800

    成员变量自动注册序列化宏开发中

--- Serialize/Serializable.h
+++ Serialize/Serializable.h
@@ -11,35 +11,39 @@ namespace bms {
         virtual Serializable* deserialize(QFile& file) = 0;
     };
 }
-// #define SERIALIZE(...) \
-    // QDataStream serialize() const override { \
-        // QDataStream ods; \
-        // serializeImpl(ods, __VA_ARGS__); \
-        // return ods; \
-    // } \
-    // void deserialize(const QDataStream& s) override { \
-        // QDataStream ids(s); \
-        // deserializeImpl(ids, __VA_ARGS__); \
-    // } \
-// private: \
-    // template<typename T> \
-    // void serializeImpl(QDataStream& ods, const T& t) const { \
-        // ods << t << " "; \
-    // } \
-    // template<typename T, typename... Args> \
-    // void serializeImpl(QDataStream& ods, const T& t, const Args&... args) const { \
-        // ods << t << " "; \
-        // serializeImpl(ods, args...); \
-    // } \
-    // template<typename T> \
-    // void deserializeImpl(QDataStream& ids, T& t) { \
-        // ids >> t; \
-    // } \
-    // template<typename T, typename... Args> \
-    // void deserializeImpl(QDataStream& ids, T& t, Args&... args) { \
-        // ids >> t; \
-        // deserializeImpl(ids, args...); \
-    // }
-
-
+/*
+#define SERIALIZE(...) \
+    void serialize() const override { \
+        QDataStream ds; \
+        if (!file.open(QIODevice::WriteOnly)) { \ 
+            std::cerr << "Failed to open file for writing" << '\n'; \
+        } \
+        ds.setDevice(&file); \
+        ds << name << age; \
+        file.close();\
+    } \
+    Serializable* deserialize(const QDataStream& s) override { \
+        QDataStream ids(s); \
+        deserializeImpl(ids, __VA_ARGS__); \
+    } \
+private: \
+    template<typename T> \
+    void serializeImpl(QDataStream& ods, const T& t) const { \
+        ods << t << " "; \
+    } \
+    template<typename T, typename... Args> \
+    void serializeImpl(QDataStream& ods, const T& t, const Args&... args) const { \
+        ods << t << " "; \
+        serializeImpl(ods, args...); \
+    } \
+    template<typename T> \
+    void deserializeImpl(QDataStream& ids, T& t) { \
+        ids >> t; \
+    } \
+    template<typename T, typename... Args> \
+    void deserializeImpl(QDataStream& ids, T& t, Args&... args) { \
+        ids >> t; \
+        deserializeImpl(ids, args...); \
+    }
+*/
 #endif // SERIALIZE_SERIALIZABLE_H
--- Serialize/test.cpp
+++ Serialize/test.cpp
@@ -1,6 +1,6 @@
 #include "User.h"
 int main() {
-    User user("John Doe", 30);
+    User user("John Ditor", 17);
     QFile file("user.dat");
     user.serialize(file);
     User* user2 = dynamic_cast<User*>(user.deserialize(file));
